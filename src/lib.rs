69a30fd64870482ed7da0bd4f6ba3f88e37906c6
jkciw <gitmailme.i5jqa@slmail.me>
2024-03-28 07:34:37 +0530

    Fmt fix

--- src/lib.rs
+++ src/lib.rs
@@ -351,12 +351,11 @@ mod test {
         assert!(!selection_output.selected_inputs.is_empty());
 
         inputs = setup_output_groups_withsequence();
-        options=setup_options(500);
+        options = setup_options(500);
         result = select_coin_fifo(&inputs, options);
         assert!(result.is_ok());
         selection_output = result.unwrap();
         assert!(!selection_output.selected_inputs.is_empty());
-
     }
 
     fn test_insufficient_funds() {
