e512fb52493e79987160a218c21bc57715be9f5c
jkciw <gitmailme.i5jqa@slmail.me>
2024-03-31 11:33:16 +0530

    Made changes w.r.t estimated_fees in accumulated_value conditionals.

--- src/lib.rs
+++ src/lib.rs
@@ -160,8 +160,7 @@ pub fn select_coin_fifo(
         estimated_fees = calculate_fee(accumulated_weight, options.target_feerate);
         if accumulated_value
             >= (options.target_value
-                + options.target_value
-                + estimated_fees
+                + estimated_fees.max(options.min_absolute_fee)
                 + options.min_drain_value)
         {
             break;
@@ -171,9 +170,9 @@ pub fn select_coin_fifo(
         selected_inputs.push(index);
     }
     if accumulated_value
-        < options.target_value
+        < (options.target_value
             + estimated_fees.max(options.min_absolute_fee)
-            + options.min_drain_value
+            + options.min_drain_value)
     {
         Err(SelectionError::InsufficientFunds)
     } else {
