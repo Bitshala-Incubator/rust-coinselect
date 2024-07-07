cd51dce0a17d490270e7d0bfc3dc245544b57785
jkciw <primefrime@protonmail.com>
2024-07-07 12:55:30 +0530

    Rebased to address signed commit issues

--- src/lib.rs
+++ src/lib.rs
@@ -325,7 +325,7 @@ pub fn select_coin_fifo(
         selected_inputs.push(index);
     }
     if accumulated_value
-        < options.target_value
+        < (options.target_value
             + estimated_fees.max(options.min_absolute_fee)
             + options.min_drain_value)
     {
@@ -698,16 +698,6 @@ mod test {
         let mut result = select_coin_srd(&inputs, options);
         assert!(result.is_ok());
         let mut selection_output = result.unwrap();
-<<<<<<< HEAD
-        assert!(!selection_output.selected_inputs.is_empty());
-
-        inputs = setup_output_groups_withsequence();
-        options = setup_options(500);
-        result = select_coin_fifo(&inputs, options);
-        assert!(result.is_ok());
-        selection_output = result.unwrap();
-=======
->>>>>>> b3e707d (Removed a redundant utility function. Substituted its function with the existing utility function)
         assert!(!selection_output.selected_inputs.is_empty());
 
         inputs = setup_output_groups_withsequence();
