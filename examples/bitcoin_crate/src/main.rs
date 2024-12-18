765df6972ce7ba5e064fe2e11d0231612be3f6e9
Harsh <115883668+NeoZ666@users.noreply.github.com>
2024-12-18 15:23:44 +0530

    Simplified the Bitcoin example file, removed the redundancy (#91)
    * Refactor to simplify
    
    * Updates to Create_outputgroup
    
    Signed-off-by: neoz666 <neoz.blockchain@gmail.com>
    
    * Various functions moved to main block, improved readibility, removed all guesswork
    
    Signed-off-by: neoz666 <neoz.blockchain@gmail.com>
    
    * Changes to documentation
    
    Signed-off-by: neoz666 <neoz.blockchain@gmail.com>
    
    * Moved to main, fixes
    
    Signed-off-by: neoz666 <neoz.blockchain@gmail.com>
    
    * Static Script
    
    Signed-off-by: neoz666 <neoz.blockchain@gmail.com>
    
    ---------
    
    Signed-off-by: neoz666 <neoz.blockchain@gmail.com>

--- examples/bitcoin_crate/src/main.rs
+++ examples/bitcoin_crate/src/main.rs
@@ -12,235 +12,170 @@ use bitcoin::{
 use rust_coinselect::{
     selectcoin::select_coin,
     types::{CoinSelectionOpt, ExcessStrategy, OutputGroup},
-    utils::calculate_base_weight_btc,
+    utils::{calculate_base_weight_btc, calculate_fee},
 };
-use serde_derive::Deserialize;
-use std::fs;
-use std::{path::Path, str::FromStr};
+use std::str::FromStr;
 
-// A struct to read and store transaction inputs from the JSON file
-#[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct TxInJson {
-    txid: String,
-    vout: u32,
-    script_sig: String,
-    sequence: String,
-    witness: Vec<String>,
-}
+fn log_utxos(utxos: &[OutputGroup]) {
+    println!(
+        "\n{:<15} | {:<15} | {:<15} | {:<20}",
+        "Value (sats)", "Weight (bytes)", "Input Count", "Creation Sequence"
+    );
+    println!("{:-<71}", "");
 
-// A struct to read and store transaction outputs from the JSON file
-#[derive(Deserialize)]
-struct TxOutJson {
-    value: f64,
-    script_pubkey: String,
-}
-
-fn read_json_file(file_path: &str) -> Result<String, Box<dyn std::error::Error>> {
-    // Checking if the given path exists
-    if !Path::new(file_path).exists() {
-        return Err(Box::new(std::io::Error::new(
-            std::io::ErrorKind::NotFound,
-            "File not found",
-        )));
-    }
-    fs::read_to_string(file_path).map_err(|e| Box::new(e) as Box<dyn std::error::Error>)
-}
-
-fn json_to_txin(filedata: &str) -> Result<Vec<TxIn>, Box<dyn std::error::Error>> {
-    // Parse transaction input data from JSON file into TxIn struct of the bitcoin crate
-    let tx_in_json_vec: Vec<TxInJson> = serde_json::from_str(filedata)?;
-    tx_in_json_vec
-        .into_iter()
-        .map(|tx_inp| {
-            let txid = Txid::from_str(&tx_inp.txid)?;
-            let script_signature = ScriptBuf::from_hex(&tx_inp.script_sig)?;
-            let nsequence = Sequence::from_hex(&tx_inp.sequence)?;
-            // Converting array of strings to slice of bytes
-            let witness: Vec<&str> = tx_inp.witness.iter().map(|w| &w[..]).collect();
-            // Converting from slice of bytes to Witness object
-            let witnessdata = Witness::from_slice(&witness);
-            Ok(TxIn {
-                previous_output: OutPoint {
-                    txid,
-                    vout: tx_inp.vout,
-                },
-                script_sig: script_signature,
-                sequence: nsequence,
-                witness: witnessdata,
-            })
-        })
-        .collect()
-}
-
-fn json_to_txout(filedata: &str) -> Result<Vec<TxOut>, Box<dyn std::error::Error>> {
-    // Parse transaction output data from JSON file into TxOut struct of the bitcoin crate
-    let tx_out_json_vec: Vec<TxOutJson> = serde_json::from_str(filedata)?;
-    tx_out_json_vec
-        .into_iter()
-        .map(|tx_op| {
-            let op_amount = Amount::from_btc(tx_op.value)?;
-            let op_script_pubkey = ScriptBuf::from_hex(&tx_op.script_pubkey)?;
-            Ok(TxOut {
-                value: op_amount,
-                script_pubkey: op_script_pubkey,
-            })
-        })
-        .collect()
-}
-
-// The 'Transaction' struct of the bitcoin crate represents an UTXO. Here, the inputs (TxIn) and outputs (TxOut) are used to construct the 'Transaction' struct.
-fn create_transaction(txinput: Vec<TxIn>, txoutput: Vec<TxOut>) -> Transaction {
-    // Create a new transaction with the given vector of inputs and outputs. Assuming version = 2 and locktime = 0
-    Transaction {
-        version: transaction::Version::TWO,
-        lock_time: LockTime::ZERO,
-        input: txinput,
-        output: txoutput,
-    }
-}
-
-// UTXO (Transaction) is a combination of inputs and outputs. Here we pick inputs and outputs from the vector of TxIn and TxOut to construct the UTXO.
-fn compose_transactions(inputs: Vec<TxIn>, outputs: Vec<TxOut>) -> Vec<Transaction> {
-    // Generate combinations of inputs and outputs and create transactions using them
-    vec![
-        // UTXO 1 : Two inputs and one output
-        create_transaction(
-            inputs.iter().take(2).cloned().collect(),
-            vec![outputs[2].clone()],
-        ),
-        // UTXO 2 : Three inputs and three outputs
-        create_transaction(
-            inputs.iter().skip(2).take(3).cloned().collect(),
-            outputs.iter().skip(3).take(3).cloned().collect(),
-        ),
-        // UTXO 3: Five inputs and five outputs
-        create_transaction(
-            inputs.iter().take(5).cloned().collect(),
-            outputs.iter().skip(1).take(5).cloned().collect(),
-        ),
-        // UTXO 4: One input and 7 outputs
-        create_transaction(
-            vec![inputs[5].clone()],
-            outputs.iter().take(7).cloned().collect(),
-        ),
-        // UTXO 5: Two inputs and one output
-        create_transaction(
-            inputs.iter().skip(4).take(2).cloned().collect(),
-            vec![outputs[6].clone()],
-        ),
-    ]
-}
-
-fn create_outputgroup(tx: Vec<Transaction>) -> Vec<OutputGroup> {
-    // Create OutputGroup from transaction data
-    tx.into_iter()
-        .enumerate()
-        .map(|(i, tx)| OutputGroup {
-            value: tx.output.iter().map(|op| op.value.to_sat()).sum(),
-            weight: tx.total_size() as u64,
-            input_count: tx.input.len(),
-            creation_sequence: Some(i as u32),
-        })
-        .collect()
-}
-
-fn create_select_options(output: &TxOut) -> Vec<CoinSelectionOpt> {
-    let target_values = [50_000, 100_000, 200_000, 500_000, 1_000_000];
-    let feerates = [1.0, 2.0, 3.0, 5.0, 10.0];
-    // Use the weight() method from TxOut
-    let base_weight = calculate_base_weight_btc(output.weight().to_wu());
-    println!("Base weight: {}", base_weight);
-
-    (0..5)
-        .map(|i| {
-            let excess_strategy = match i % 3 {
-                0 => ExcessStrategy::ToChange,
-                1 => ExcessStrategy::ToFee,
-                2 => ExcessStrategy::ToRecipient,
-                _ => unreachable!(),
-            };
-
-            CoinSelectionOpt {
-                target_value: target_values[i],
-                target_feerate: feerates[i],
-                long_term_feerate: Some(10.0),
-                min_absolute_fee: 1000 * (i + 1) as u64,
-                base_weight,
-                change_weight: 34,
-                change_cost: 5 + i as u64,
-                avg_input_weight: 148,
-                avg_output_weight: 34,
-                min_change_value: 100,
-                excess_strategy,
-            }
-        })
-        .collect()
-}
-
-fn perform_select_coin(utxos: Vec<OutputGroup>, coin_select_options_vec: Vec<CoinSelectionOpt>) {
-    // Printing information about the UTXOs used for selection
-    println!("\nThe total number of UTXOs available: {:?}", utxos.len());
-    for (i, utxo) in utxos.iter().enumerate() {
-        println!("\nUTXO #:{}", i);
-        println!("Value:{} sats", utxo.value);
-        println!("Weight:{} bytes", utxo.weight);
-        println!("No. of Inputs: {}", utxo.input_count);
+    for utxo in utxos.iter() {
         println!(
-            "Creation Sequence: {:?}",
+            "{:<15} | {:<15} | {:<15} | {:<20}",
+            utxo.value,
+            utxo.weight,
+            utxo.input_count,
             utxo.creation_sequence.unwrap_or(0)
         );
     }
-
-    for coin_select_options in coin_select_options_vec.iter().take(5) {
-        println!(
-            "\nSelecting UTXOs to total: {:?} sats",
-            coin_select_options.target_value
-        );
-        match select_coin(&utxos, coin_select_options) {
-            Ok(selectionoutput) => {
-                println!(
-                    "Selected utxo index and waste metrics are: {:?}",
-                    selectionoutput
-                );
-            }
-            Err(e) => {
-                println!("Error performing coin selection: {:?}", e);
-            }
-        }
-    }
+    println!("{:-<71}", "");
 }
 
 fn main() {
-    // Read and parse inputs from JSON file
-    let inputs = match read_json_file("examples/bitcoin_crate/txdata/txinp.json")
-        .and_then(|filedata| json_to_txin(&filedata))
-    {
-        Ok(txin_vec) => txin_vec,
-        Err(e) => {
-            println!("Error reading or parsing inputs: {:?}", e);
-            return;
+    let target: u64 = 4_000_000;
+
+    let inputs: Vec<TxIn> = vec![
+        TxIn {
+            previous_output: OutPoint {
+                txid: Txid::from_str("e9269b40306f10e2636414e514366474d5736256844882ffefd794f688b648b0").unwrap(),
+                vout: 0,
+            },
+            script_sig: ScriptBuf::from_hex("47304402200552d42d255d9814bda2e0fbf28be6321a80615e633afb21d18abf4ad0bc721602202530e3f8e4bbf184eecfd95c615cb22ad06860fd75ce3aa423d0fea894a80332812103739ea9368ff2b1fdc4db2f160191e980190367501cc2b0c93a566fababd01064").unwrap(),
+            sequence: Sequence::from_hex("0xffffffff").unwrap(),
+            witness: Witness::default()
+        },
+        TxIn {
+            previous_output: OutPoint {
+                txid: Txid::from_str("e1926a751f36c25541263ca4b621f1e3376e15117c170e60ba82b113bda3cba6").unwrap(),
+                vout: 0,
+            },
+            script_sig: ScriptBuf::from_hex("47304402203743aeedace4ec68892c825ed674234b7c834268304aaaf76958d2da5670a9b702207b01a50a5c047023ac5a5b90367b1bd862ac6a4914b023a7c5463b3f8e3690710141047146f0e0fcb3139947cf0beb870fe251930ca10d4545793d31033e801b5219abf56c11a3cf3406ca590e4c14b0dab749d20862b3adc4709153c280c2a78be10c").unwrap(),
+            sequence: Sequence::from_hex("0xffffffff").unwrap(),
+            witness: Witness::default()
+        },
+        TxIn {
+            previous_output: OutPoint {
+                txid: Txid::from_str("0c20171f1558eb6c032a5916e8ce0b446fef6f7b102b2aabcfc83a8367b0f53b").unwrap(),
+                vout: 2,
+            },
+            script_sig: ScriptBuf::default(),
+            sequence: Sequence::from_hex("0xfffffffe").unwrap(),
+            witness: Witness::from_slice(&[
+                "30440220079287854aed2500913921a7b9698cefc17fc68fb02728d2e86c52173b6af6ba0220669efe80b73204efcda6c3a375da5fd059977e6c86bad977b15c562219cc5dd601",
+                "032d49f270684da5a422df37bf9818aa178ad89a975643c4928444aed78a7dd341"
+            ]),
+        },
+        TxIn {
+            previous_output: OutPoint {
+                txid: Txid::from_str("f2ccf54fa95a13b092d4f90c9cb215d11c858fd2964a45f3f997b3d4815e24e4").unwrap(),
+                vout: 1,
+            },
+            script_sig: ScriptBuf::default(),
+            sequence: Sequence::from_hex("0xfffffffd").unwrap(),
+            witness: Witness::from_slice(&[
+                "30450221008c10e822049f4e67388cd9dc825d1bf14c7f8b73801bc025e7534b33319d510f02205c214beb89a3b3e0837b6cc717dbda4f8707076050197b870cffec7594040c8801",
+                "02f2a0bdce72551e68dfbebf81d770924836cbb256a63e9987ea869eabac4859c1"
+            ]),
         }
+    ];
+
+    // Pre-calculate transaction weight using a dummy change output
+    // Rationale:
+    // 1. Transaction weight calculation requires both inputs and outputs
+    // 2. We know the target output value (4M sats) but not the change amount yet
+    // 3. Using 0 as placeholder for change output is valid because:
+    //    - Output weight in Bitcoin depends on script size, not value
+    //    - Value field is fixed 8 bytes regardless of amount
+    //    - Only the script pubkey size affects weight
+    let mut change_value = 0;
+    const SCRIPT: &str = "a91409f6eed90e2ec7fed923b3d0b9d026efded6335c87";
+
+    fn change_output(change_value: u64) -> TxOut {
+        TxOut {
+            value: Amount::from_sat(change_value),
+            script_pubkey: ScriptBuf::from_hex(SCRIPT).unwrap(),
+        }
+    }
+    let target_output = TxOut {
+        value: Amount::from_sat(target),
+        script_pubkey: ScriptBuf::from_hex(SCRIPT).unwrap(),
+    };
+    let outputs = vec![target_output.clone(), change_output(change_value)];
+
+    fn create_tx(inputs: Vec<TxIn>, outputs: Vec<TxOut>) -> Transaction {
+        Transaction {
+            version: transaction::Version::TWO,
+            lock_time: LockTime::ZERO,
+            input: inputs,
+            output: outputs,
+        }
+    }
+
+    let output_weight_sum = outputs.iter().map(|output| output.weight().to_wu()).sum();
+
+    // Create coin selection options
+    let coin_selection_option = CoinSelectionOpt {
+        target_value: target,
+        target_feerate: 5.0,
+        long_term_feerate: Some(10.0),
+        min_absolute_fee: 4000,
+        base_weight: calculate_base_weight_btc(output_weight_sum),
+        change_weight: 34,
+        change_cost: 8,
+        avg_input_weight: inputs
+            .iter()
+            .map(|input| u64::from(input.segwit_weight()))
+            .sum::<u64>()
+            / inputs.len() as u64,
+        avg_output_weight: output_weight_sum / 2,
+        min_change_value: 100,
+        excess_strategy: ExcessStrategy::ToChange,
     };
 
-    // Read and parse outputs from JSON file
-    let outputs = match read_json_file("examples/bitcoin_crate/txdata/txop.json")
-        .and_then(|filedata| json_to_txout(&filedata))
-    {
-        Ok(txout_vec) => txout_vec,
-        Err(e) => {
-            println!("Error reading or parsing outputs: {:?}", e);
-            return;
+    // Mock values for each input
+    let mock_input_values = vec![100_000, 500_000, 1_000_000, 3_000_000];
+
+    // Create OutputGroups from each input
+    let utxos: Vec<OutputGroup> = inputs
+        .clone()
+        .into_iter()
+        .zip(mock_input_values)
+        .map(|(input, value)| OutputGroup {
+            // In practice, the details about the UTXO, used as input, is obtained from the UTXO set maintained by a node.
+            value,
+            weight: u64::from(input.segwit_weight()),
+            input_count: 1,
+            creation_sequence: None,
+        })
+        .collect();
+
+    // Perform selection among the available UTXOs, create final transaction
+    log_utxos(&utxos);
+    match select_coin(&utxos, &coin_selection_option) {
+        Ok(selection) => {
+            println!("Selected utxo index and waste metrics are: {:?}", selection);
+            let selected_utxo_aggregate: u64 = selection
+                .selected_inputs
+                .iter()
+                .map(|&i| utxos[i].value)
+                .sum();
+
+            // Create a transaction for the purpose of calculating a default weight first
+            let fee = calculate_fee(
+                u64::from(create_tx(inputs.clone(), outputs).weight()),
+                coin_selection_option.long_term_feerate.unwrap(),
+            );
+
+            change_value = selected_utxo_aggregate - (target + fee);
+            println!(
+                "Transaction Outputs created with target: {target} and change: {change_value}"
+            );
+            create_tx(inputs, vec![target_output, change_output(change_value)]);
         }
-    };
-
-    // Create new transactions using all possible combinations of inputs and outputs
-    let transactions = compose_transactions(inputs.clone(), outputs.clone());
-    // Create UTXOs of type OutputGroup to be passed to coin selection
-    let utxos = create_outputgroup(transactions);
-    // Create options for coin selection - modify to pass reference
-    let coin_selection_options = create_select_options(&outputs[0]);
-
-    // Perform coin selection
-    perform_select_coin(utxos, coin_selection_options);
+        Err(_) => println!("Coin Selection failed!"),
+    }
 }
